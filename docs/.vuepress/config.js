6016ac7262b39e3caa64fd349e655474a851c0ef
panzhiyue <892105346@qq.com>
2022-05-16 11:59:25 +0800

    修改文档根节点名称

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   // theme: 'vdoing', // 使用依赖包主题
   title: "vue2ol",
   description: 'openlayers的vue组件',
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+   base: '/vue2ol/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', {
       rel: 'icon',
