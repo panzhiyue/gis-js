372d2537869c3b6df436a85482d1513b505aed82
panzhiyue <30484534+panzhiyue@users.noreply.github.com>
2022-05-17 16:30:22 +0800

    给Layer添加source属性

--- packages/vue2ol/src/mixins/Layer.js
+++ packages/vue2ol/src/mixins/Layer.js
@@ -2,7 +2,18 @@ import BaseLayerMixin from './BaseLayer'
 
 export default {
   mixins: [BaseLayerMixin],
+  props: {
+    /**
+     * 
+     */
+    source: {
+      type: Object,
+    },
+  },
   mounted() {
-    this.layerOptions = this.baseLayerOptions
+    this.layerOptions = {
+      ...(this.baseLayerOptions || {}),
+      source: this.source,
+    }
   }
 }
