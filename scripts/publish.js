bf062f744df1b6fd7d57ec13b5cee4cfa0b10ec0
panzhiyue <30484534+panzhiyue@users.noreply.github.com>
2022-05-23 10:28:39 +0800

    暂存

--- scripts/publish.js
+++
@@ -1,7 +0,0 @@
-const shell = require('shelljs');
-const path = require("path")
-
-shell.exec('lerna version' );
-shell.exec(path.join(__dirname, '../bin/npmjs.sh') );
-const lerna = require('../lerna.json')
-shell.exec(path.join(__dirname, '../bin/tag.sh'),lerna.version );
