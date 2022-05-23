e9d215046cbeb165f3f48fa340d6bd5958120ac0
panzhiyue <30484534+panzhiyue@users.noreply.github.com>
2022-05-23 09:58:35 +0800

    暂存

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,4 +3,14 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-lerna publish
+cd packages/utilsol
+npm run build:package
+npm publish --access=public
+
+cd ../vue2ol
+npm run build:package
+npm publish --access=public
+
+cd ../vue2ol-extend
+npm run build:package
+npm publish --access=public
